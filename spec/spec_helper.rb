5ca4069b0c163e505edf7b2335a91adac1f2ffca
Mahmoud Ali <m7moud.said@gmail.com>
2015-11-08 00:14:37 +0200

    Fix for "Client has a version number"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'shore/customers/client'
+require 'shore/customers'
 
 # Load support files
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
